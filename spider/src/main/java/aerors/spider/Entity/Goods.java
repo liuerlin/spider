01d40867c8c6b7de4c3d6b6a9046956c52bd3f64
liuerlin <18092856815@163.com>
2019-03-26 17:46:27 +0800

    增加了goods映射

---
+++ spider/src/main/java/aerors/spider/Entity/Goods.java
@@ -0,0 +1,27 @@
+package aerors.spider.Entity;
+
+public class Goods {
+    private Integer id;
+    private String code;
+
+    public Goods(Integer id, String code) {
+        this.id = id;
+        this.code = code;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
